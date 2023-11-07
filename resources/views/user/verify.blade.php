83c95090ecec993360d79919fad71d61253c38a9
Phạm Quang Lộc <locmaymo@gmail.com>
2023-11-07 18:28:10 +0700

    fgs

--- resources/views/user/verify.blade.php
+++ resources/views/user/verify.blade.php
@@ -9,7 +9,7 @@
             <div class="card">
                 <div class="card-header">Xác nhận địa chỉ email</div>
                 <div class="card-body">
-                    <p>Hãy xác nhận địa chỉ email trong hộp thư của bạn trước</p>
+                    <p>Hãy xác nhận địa chỉ email trong hộp thư của bạn trước!</p>
 
                     <a href="{{route('resend.email')}}">Gửi lại email xác minh</a>
                 </div>
