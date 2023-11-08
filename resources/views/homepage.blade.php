d35049c8e103983a0e174b2d69569af8e475cabb
Phạm Quang Lộc <locmaymo@gmail.com>
2023-11-08 18:57:26 +0700

    trang chu

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,4 +2,23 @@
 
 @section('content')
     <h1 class="text-center mt-5">TRANG CHU =)))</h1>
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci animi assumenda atque beatae consequatur
+        consequuntur cumque delectus deserunt doloremque doloribus eligendi eos error esse ex exercitationem facere
+        fugiat fugit harum id illo illum impedit inventore ipsa ipsum iste itaque laboriosam laborum libero magnam
+        maiores maxime molestias mollitia nam natus necessitatibus nemo nesciunt nisi nobis non nostrum nulla odio
+        officiis pariatur perferendis perspiciatis porro possimus quae quia quibusdam quisquam quod repellat rerum
+        saepe sed similique, sunt tempora tenetur ullam unde velit veniam voluptas voluptatibus voluptatum. Accusamus
+        accusantium aliquid architecto asperiores aspernatur atque autem, blanditiis commodi consequatur consequuntur
+        deleniti dicta distinctio dolor dolore doloremque doloribus ducimus eius eos error esse est exercitationem
+        explicabo facere facilis fugiat fugit harum hic id illum impedit in incidunt iusto labore laborum libero
+        magnam maiores maxime molestiae molestias natus necessitatibus nesciunt nihil nisi nobis non nostrum nulla
+        numquam obcaecati odio omnis pariatur perferendis perspiciatis placeat praesentium provident quae quas qui
+        quibusdam quidem quis, quod quos reiciendis repellat repellendus reprehenderit rerum saepe sapiente similique
+        sit tempora temporibus tenetur totam ullam unde vel voluptas voluptate voluptatum. Accusantium ad alias
+        aperiam, architecto asperiores aspernatur atque aut consequatur consequuntur cupiditate debitis deleniti
+        distinctio dolorum ducimus eius eos esse est et eveniet ex excepturi exercitationem explicabo facilis fuga
+        fugit harum hic id impedit in ipsa iure laboriosam laborum libero magnam maxime minima minus modi molestias
+        mollitia nam natus nemo neque nihil nisi non nostrum numquam obcaecati officiis optio
+    </p>
 @endsection
